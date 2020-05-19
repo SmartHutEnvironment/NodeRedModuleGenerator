83a22bc58376eb44dc0ff78f9b7a3389a40550a1
Ferenc Klein <ferenc.klein.pe@gmail.com>
2020-05-19 20:40:05 +0200

    improvements

--- src/generator/configFileGenerator.py
+++ src/generator/configFileGenerator.py
@@ -1,11 +1,22 @@
 import yaml;
 import json;
+import os.path;
+import re;
 
 class NodeConfig:
 	def __init__(self, configFilePath):
+		self.path = os.path.dirname(configFilePath);
 		configFile = open(configFilePath);
 		self.config = yaml.load(configFile.read(), Loader=yaml.SafeLoader)["node"];
 		configFile.close();
+
+		self.htmlScript = None;
+		htmlPath = os.path.dirname(configFilePath) + "/html.js";
+		print(htmlPath);
+		if os.path.exists(htmlPath):
+			script = open(htmlPath);
+			self.htmlScript = script.read();
+			script.close();
 
 	def GenerateSchema(self):
 		result = {};
@@ -68,7 +79,27 @@ class NodeConfig:
 	def GenerateConfig(self):
 		schema = self.GenerateSchema();
 
-		script = f"""
+		script = "";
+
+		if self.htmlScript != None:
+			match = re.search("\@include\(\"(.*?)\"\)", self.htmlScript);
+			while match:
+				target = self.path + "/" + match.group(1);
+				if os.path.exists(target):
+					f = open(target);
+					text = f.read();
+					f.close();
+					self.htmlScript = self.htmlScript.replace(match.group(0), text);
+				else:
+					print("File not found: ", target);
+				match = re.search("\@include\(\"(.*?)\"\)", self.htmlScript);
+
+			script += f"""
+<script type="text/javascript">
+{self.htmlScript}
+</script>""";
+
+		script += f"""
 <script type="text/javascript">
 RED.nodes.registerType('{self.config["id"]}', {schema});
 </script>
--- src/generator/jsFileGenerator.py
+++ src/generator/jsFileGenerator.py
@@ -1,4 +1,5 @@
 import os.path;
+import re;
 
 class JsFileGenerator:
 	def __init__(self, path, jsFile, config, resources):
@@ -14,6 +15,7 @@ class JsFileGenerator:
 
 		schema = f"""let RED;
 const state = module.exports = function(red) {{
+	RED = red;
 	red.nodes.registerType("{self.config["id"]}", {self.className});
 }}
 
@@ -26,6 +28,18 @@ const state = module.exports = function(red) {{
 
 		for name in self.resources:
 			data = data.replace("@resource:" + name, self.resources[name]);
+
+		match = re.search("\@include\(\"(.*?)\"\)", data);
+		while match:
+			target = self.path + "/" + match.group(1);
+			if os.path.exists(target):
+				f = open(target);
+				text = f.read();
+				f.close();
+				data = data.replace(match.group(0), text);
+			else:
+				print("File not found: ", target);
+			match = re.search("\@include\(\"(.*?)\"\)", data);
 
 		return data;
 
